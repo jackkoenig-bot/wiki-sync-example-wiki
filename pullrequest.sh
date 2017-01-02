eee7aa39e70fe19e5f9ee4c6b4f08cc3765bafe0
Jack <jack.koenig3.bot@gmail.com>
2017-01-02 14:06:31 -0600

    For PRs, check for clean merge

--- pullrequest.sh
+++ pullrequest.sh
@@ -1,1 +1,7 @@
-echo "Travis on a PR!"
+# Just check for clean merge
+git remote remove origin
+git remote add origin https://github.com/jackkoenig-bot/wiki-sync-example-wiki.git
+git remote add upstream https://github.com/jackkoenig-bot/wiki-sync-example.wiki.git
+git fetch origin
+git fetch upstream
+git merge upstream/master --no-edit
